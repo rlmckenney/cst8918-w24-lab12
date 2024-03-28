e2b2ed0d8668d9908c049bff937a44dae1ca5a70
Robert McKenney <robert@mckennehy.ca>
2024-03-28 12:34:21 -0400

    FIX: RG pointer

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -9,14 +9,14 @@ resource "azurerm_resource_group" "app" {
 }
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.resource_prefix}-vnet"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.app.name
+  location            = azurerm_resource_group.app.location
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "${var.resource_prefix}-test-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.app.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
